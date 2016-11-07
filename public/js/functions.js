b51f811558f6ee9d4b64070ec45dec4dfaa1faa4
Gerrit Neven <gerrit@hertzian.co.uk>
2016-11-07 12:04:14 +0000

    toying around with headers

--- public/js/functions.js
+++ public/js/functions.js
@@ -2,14 +2,18 @@
 var allCameras = [];
 var count = 0;
 
-// $(document).ready(function(){
+$(document).ready(function(){
     
-//     $.ajax({
-//         url: "allcameras.json",
-//         dataType: "jsonp"
-//     });
+    $.ajax({
+        method: 'GET',
+        url: 'http://requestb.in/wbocqbwb',
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Connection', 'keep-alive');
+            xhr.setRequestHeader('Keep-Alive', 'timeout=9999, max=1000');
+        }
+    });
 
-// });
+});
 
 function changeImage(selector, imageSrc) {		
     count += 1;
