c577ead1c6d1b40e6245e80c86c81f57971488b5
plymouthmarina <marina.trethewey@gmail.com>
2016-11-15 22:17:31 +0000

    Access Shodan query results past the 1st page. For every 100 results past the 1st page 1 query credit is deducted. Currently processing 827 webcamxp IP addresses with 105 being viable.

--- public/js/functions.js
+++ public/js/functions.js
@@ -25,40 +25,9 @@ function jsonCallback(json){
 
     allCameras = json;
 
-    window.setInterval(function(){
+    window.setInterval(function() {
         changeImage('#cam1', allCameras[0]);
         changeImage('#cam2', allCameras[1]);
         changeImage('#cam3', allCameras[2]);
     }, 1000);
-
 }
-
-
-
-
-/*
-
-function readTextFile(file) {
-
-    var xmlhttp = new XMLHttpRequest();
-    
-    xmlhttp.open("GET", file, true);
-
-    xmlhttp.onreadystatechange = function ()
-    {
-        if(xmlhttp.readyState === 4)
-        {
-            if(xmlhttp.status === 200 || xmlhttp.status == 0)
-            {
-                var allText = xmlhttp.responseText;
-                allCameras.push(allText);
-                console.log(allCameras);
-            }
-        }
-    }
-
-    xmlhttp.send(null);
-
-}
-
-*/
--- server/cameras.js
+++ server/cameras.js
@@ -3,6 +3,7 @@ var request = require('request');
 var jimp = require('jimp');
 const client = require('shodan-client');
 const redis = require('redis');
+var util = require('util');
 
 const r = redis.createClient();
 
@@ -14,7 +15,7 @@ function cameras(app) {
   return app.get('/cameras', function (req, res) {
     console.log('starting');
     var allCameras = [];
-    // create a queue object with concurrency 5
+    // create a queue object with concurrency 10
     var q = async.queue(function(url, callback) {
       console.log('Scanning: ' + url);
 
@@ -30,8 +31,8 @@ function cameras(app) {
     }, 10);
 
     q.drain = function(){
-      console.log('all items have been processed');
-      return res.json({'cameras': allCameras});
+        console.log('Queue empty');
+        return res.send('Finished');
     };
 
     var processIp = function(url) {
@@ -51,27 +52,68 @@ function cameras(app) {
       });
     };
 
-    const searchOpts = {
-      // facets: 'port:100,country:100',
-      // minify: false,
+    var urlsToCheck = [];
+
+    var searchCameras = function(page)
+    {
+      var searchOpts = {
+        page: page,
+        timeout: 5000
+      };
+
+      console.log('Processing page: ' + page);
+
+      client.search('webcamxp', 'zyEqgrpVl4T6zPxajV7q4rxB0vvFKoRm', searchOpts)
+      .then(res => {
+        //console.log('Result:');
+        // console.log(util.inspect(res, { depth: 6 }));
+        for (var i = 0; i < res.matches.length; i++) {
+          var ipStr = res.matches[i].ip_str;
+          var lat = res.matches[i].location.latitude;
+          var long = res.matches[i].location.longitude;
+          console.log("IP = " + ipStr + ", Latitude = " + lat + ", Longitude = " + long);
+          // var j = 1;
+          for (var j = 1; j < 4; j++) {
+            var url = 'http://' + ipStr + ':8080/cam_' + j + '.jpg';
+            // processIp(url);
+            urlsToCheck.push(url);
+          }
+        }
+
+        // Try the next page if there is one.
+        var pages = Math.ceil(res.total / 100);
+
+        // There is another page.
+        if(page < pages)
+        {
+          searchCameras(page + 1);
+        }
+        // Already on the last page, so process URLs.
+        else
+        {
+          for(var i = 0; i < urlsToCheck.length; i++)
+          {
+            var url = urlsToCheck[i];
+            processIp(url);
+          }
+        }
+      })
+      .catch(function(e)
+      {
+        // There was an error - probably a timeout, so try again in a second.
+        console.log('Error - trying again...');
+
+        setTimeout(function()
+        {
+            searchCameras(page);
+        }, 1000);
+      });
+
     };
 
-    client.search('webcamxp', 'zyEqgrpVl4T6zPxajV7q4rxB0vvFKoRm', searchOpts)
-    .then(res => {
-      //console.log('Result:');
-      //console.log(util.inspect(res, { depth: 6 }));
-      for (var i = 0; i < res.matches.length; i++) {
-        var ipStr = res.matches[i].ip_str;
-        var lat = res.matches[i].location.latitude;
-        var long = res.matches[i].location.longitude;
-        console.log("IP = " + ipStr + ", Latitude = " + lat + ", Longitude = " + long);
-        // var j = 1;
-        for (var j = 1; j < 4; j++) {
-          processIp('http://' + ipStr + ':8080/cam_' + j + '.jpg');
-        }
-      }
+    // Start with the first page of results.
+    searchCameras(1);
 
-    });
   })
 }
 
