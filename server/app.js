9f67b4c06b0601cefda77149390c747e64f5b0f8
Gerrit Neven <gerrit@hertzian.co.uk>
2016-12-13 21:09:08 +0000

    update deployment scripts and other stuff

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const util   = require('util');
 const winston = require('winston');
 const expressWinston = require('express-winston');
 
-const client = require('socket.io-client')('http://localhost:8081');
+const client = require('socket.io-client')('http://139.59.168.137:8081');
 const socketio = require('socket.io');
 const opbeat = require('opbeat').start({
   appId: '7175bcb323',
@@ -28,15 +28,15 @@ app.use(expressWinston.errorLogger({
   ]
 }));
 
-app.use(express.static('public'));
+app.use(express.static('../public'));
 app.set('PORT', 80);
 // app.set('socket', socket);
 app.set('client', client);
 app.set('logger', winston);
 
 // Register API endpoints
-require('./cameras')(app);
-require('./feeds')(app);
+// require('./cameras')(app);
+// require('./feeds')(app);
 
 server.listen(3000, function () {
     winston.info('App listening on 3000');
